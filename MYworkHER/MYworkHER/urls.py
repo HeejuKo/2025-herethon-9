b0a08195b1176e0921514362a0660d11cc8fa143
onlyoneprivate <pllm098@naver.com>
2025-07-07 15:05:14 +0900

    ✨Feat: matching URL 경로 등록

--- MYworkHER/MYworkHER/urls.py
+++ MYworkHER/MYworkHER/urls.py
@@ -21,5 +21,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('matching.urls')),
+    path('matching/', include('matching.urls')),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT) # 이미지 업로드를 위한 설정 추가
--- MYworkHER/matching/urls.py
+++ MYworkHER/matching/urls.py
@@ -4,5 +4,6 @@ from .views import *
 app_name = 'matching'
 
 urlpatterns = [
-    
+    path('', create_matching, name='create-matching'),
+    path('success/', matching_success, name='matching-success'),
 ]
