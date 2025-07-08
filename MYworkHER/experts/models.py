fd6e6ea5a758dee49eecea36db9c8ee1f51982a2
onlyoneprivate <pllm098@naver.com>
2025-07-08 10:16:37 +0900

    ✨Feat: 전문가 프로필 모델 정의

--- MYworkHER/experts/models.py
+++ MYworkHER/experts/models.py
@@ -1,3 +1,36 @@
+from django.utils import timezone
 from django.db import models
+from matching.models import Matching
+from accounts.models import User
 
-# Create your models here.
+class CategoryChoices(models.TextChoices):
+    APPLIANCE = 'APPLIANCE', '가전/수리'
+    HEALTH = 'HEALTH', '헬스/스포츠'
+    BUSINESS = 'BUSINESS', '컨설팅/비지니스'
+    LIFESTYLE = 'LIFESTYLE', '생활/라이프'
+
+class BadgeChoices(models.TextChoices):
+    NONE = '', '없음'
+    VERIFIED = 'VERIFIED', '인증배지'
+
+class Expert(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='expert_profile')
+    category = models.CharField(max_length=20, choices=CategoryChoices.choices, null=False)
+    bio = models.TextField(max_length=200)
+    description = models.TextField()            # 업무 상세 설명
+    experience = models.IntegerField()          # 경력 (단위 : 년)
+    badge = models.CharField(max_length=20, choices=BadgeChoices.choices, default=BadgeChoices.NONE)
+
+    # 한 달 예약 수
+    @property
+    def monthly_matching_count(self):
+        today = timezone.now()
+        return Matching.objects.filter(
+            expert = self.user,
+            status = 'COMPLETE',
+            date__year = today.year,
+            date__month = today.month
+        ).count()
+
+    def __str__(self):
+        return f'{self.user.nickname} - {self.category}'
