738ee040696f01d301d9bd411554da8717d0a2cc
HeejuKo <khjg00gle96@gmail.com>
2025-07-16 00:45:43 +0900

    🐛 Bugfix: 전문가 찾기 필터링 버그 수정
    Bugfix: 전문가 찾기 필터링 버그 수정

--- MYworkHER/experts/views.py
+++ MYworkHER/experts/views.py
@@ -37,16 +37,44 @@ def expert_main(request):
         'recommended_experts': recommended_experts
     })
 
+def apply_region_filter(queryset, region_filters, seoul_all):
+    if region_filters and not seoul_all:
+        region_values = list(filter(None, (get_region_value_by_label(label) for label in region_filters)))
+        return queryset.filter(user__region__in=region_values)
+    return queryset
+
+def apply_experience_filter(queryset, experience_filters):
+    if experience_filters:
+        queries = Q()
+        for e in experience_filters:
+            if e == '0':
+                queries |= Q(experience__lt=1)
+            elif e == '1':
+                queries |= Q(experience__gte=1, experience__lt=3)
+            elif e == '3':
+                queries |= Q(experience__gte=3, experience__lt=5)
+            elif e == '5':
+                queries |= Q(experience__gte=5)
+        return queryset.filter(queries)
+    return queryset
+
+def apply_badge_filter(queryset, badge_filter):
+    if badge_filter == 'true':
+        return queryset.filter(badge=BadgeChoices.VERIFIED)
+    return queryset
+
 @login_required
 def expert_search(request):
     keyword = request.GET.get('keyword', '')
     region_filters = request.GET.getlist('region')
     experience_filters = request.GET.getlist('experience')
     badge_filter = request.GET.get('badge')
+    seoul_all = request.GET.get('seoul_all') == 'true'
+    show_all = request.GET.get('show_all') == 'true'
 
     experts_qs = Expert.objects.select_related('user').all()
 
-    # 🔍 키워드 및 카테고리 필터링
+    # 🔍 키워드 검색
     if keyword:
         category_val = get_category_value_by_label(keyword)
         category_q = Q(category=category_val) if category_val else Q()
@@ -57,35 +85,23 @@ def expert_search(request):
             category_q
         )
 
-    # 🏙️ 지역 필터링
-    if region_filters:
-        region_values = [get_region_value_by_label(label) for label in region_filters if get_region_value_by_label(label)]
-        experts_qs = experts_qs.filter(user__region__in=region_values)
+    # 🔎 필터 적용
+    experts_qs = apply_region_filter(experts_qs, region_filters, seoul_all)
+    experts_qs = apply_experience_filter(experts_qs, experience_filters)
+    experts_qs = apply_badge_filter(experts_qs, badge_filter)
 
-    # 💼 경력 필터링
-    if experience_filters:
-        queries = Q()
-        for e in experience_filters:
-            if e == '0': queries |= Q(experience__lt=1)
-            elif e == '1': queries |= Q(experience__gte=1, experience__lt=3)
-            elif e == '3': queries |= Q(experience__gte=3, experience__lt=5)
-            elif e == '5': queries |= Q(experience__gte=5)
-        experts_qs = experts_qs.filter(queries)
-
-    # 🏅 인증 배지 필터링
-    if badge_filter == 'true':
-        experts_qs = experts_qs.filter(badge=BadgeChoices.VERIFIED.value)
-
-    experts = list(experts_qs)
-    total_count = len(experts)
+    total_count = experts_qs.count()
+    experts = list(experts_qs[:10]) if not show_all else list(experts_qs)
 
     return render(request, 'experts/expert_search.html', {
         'experts': experts,
         'total_count': total_count,
         'keyword': keyword,
-        'region_filters': region_filters,
-        'experience_filters': experience_filters,
-        'badge_filter': badge_filter,
+        'selected_region': region_filters,
+        'selected_experience': experience_filters,
+        'badge': badge_filter == 'true',
+        'show_all': show_all,
+        'seoul_all': seoul_all,
         'region_list': [r[1] for r in RegionChoices.choices],
         'experience_options': [
             ('0', '1년 미만'),
